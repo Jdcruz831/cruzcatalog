516da57730dfff99ae9a9d69006434fd42ee62c7
Jdcruz831 <jacob.cruz.9353@gmail.com>
2023-10-17 16:24:07 -0700

    maybe work

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 
-
-
-
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import AboutMe from "./aboutMe";
 import Home from "./home";
 import ProjectRepository from "./projectRepository";
 import PersonalLibrary from "./personalLibrary";
 
-
 function App() {
-
   return (
-
-      <BrowserRouter>
-          <Routes>
-            <Route path = "/" element = {<Home />} />
-            <Route path = "/aboutMe" element = {<AboutMe />} />
-            <Route path = "/projectRepository" element = {<ProjectRepository />} />
-            <Route path = "/personalLibrary" element = {<PersonalLibrary />} />
-            <Route path = "/aboutMe/contact" element = {<AboutMe initialTab="5" />} />
-            
-          </Routes>
-    </BrowserRouter>
-
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutMe" element={<AboutMe />} />
+        <Route path="/projectRepository" element={<ProjectRepository />} />
+        <Route path="/personalLibrary" element={<PersonalLibrary />} />
+        <Route path="/aboutMe/contact" element={<AboutMe initialTab="5" />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
 export default App;
+
